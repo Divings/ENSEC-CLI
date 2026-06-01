efc5417eb0cf0392a95fcaf48554f39f5cb275da
Divings <144668090+Divings@users.noreply.github.com>
2026-06-01 15:39:51 +0900

    各種バッチファイルにコメントを追加

--- Package-Sources/Compile.bat
+++ Package-Sources/Compile.bat
@@ -1,9 +1,13 @@
 @echo off
+rem メインの作業ディレクトリへ移動
 cd /d ./ENSEC-CLI
+
+rem 一時ディレクトリを削除
 rmdir /s /q build
 rmdir /s /q dist
 rmdir /s /q ensec_cli.egg-info
 
+rem ビルド&チェック
 python -m build
 twine check dist/*
 pause
--- Package-Sources/Upload.bat
+++ Package-Sources/Upload.bat
@@ -1,4 +1,7 @@
 @echo off
+rem 作業ディレクトリに移動
 cd /d ./ENSEC-CLI
+
+rem パッケージをアップロード
 python -m twine upload dist/*
 pause
